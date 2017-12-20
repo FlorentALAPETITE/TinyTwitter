7fd7abe92273cffcc28ef33ab17828d1adcd0106
Alexandre BOUDINE <E166228H@I012V7pc04.ensinfo.sciences.univ-nantes.prive>
2017-12-20 14:37:23 +0100

    Added query time display

--- war/static/script.js
+++ war/static/script.js
@@ -24,6 +24,9 @@ var app = angular.module('twitt', ['ngCookies']).controller('TTController', ['$s
 
     $scope.timelineTitle = "My timeline";
 
+    $scope.execution_time_timeline;
+    $scope.execution_time_post;
+
 	$scope.redirect = function(template){
 		var url = document.URL;
 		var host = url.substring(0,url.lastIndexOf("/"));
@@ -75,23 +78,28 @@ var app = angular.module('twitt', ['ngCookies']).controller('TTController', ['$s
     }
     
     $scope.postMessage = function(){
+       var timeStart = new Date().getTime();
        gapi.client.tinyTwitterEndpoint.insertNewMessage({
          message: $scope.stext,
          userId: $cookies.userId,
          username: $cookies.username
        }).execute(
          function(resp){
+           $scope.execution_time_post = (new Date().getTime()) - timeStart;
+           $scope.stext = null;
            $scope.listMessages();
            $scope.apply();
          });
     }
     
     $scope.listMessages = function(userId=$scope.userId, username=$scope.username, messageLimit=5, replace=true){
+      var timeStart = new Date().getTime();
       gapi.client.tinyTwitterEndpoint.getTimeline({
         userId: userId,
         messageLimit: messageLimit
       }).execute(
         function(resp){
+          $scope.execution_time_timeline = (new Date().getTime()) - timeStart;
         	if (replace){
         		// changement de timeline
           		$scope.messages = resp.items;
